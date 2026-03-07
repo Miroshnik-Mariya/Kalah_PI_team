83839adabe535f3c1bbee9dadda97935111d9730
Miroshnik-Mariya <mariya3027545@gmail.com>
2026-03-07 22:09:31 +0400

    Окно "Об авторах" и горизонтальный формат
    Сделана горизонтальная развёртка и информационное окно "Об авторах".

--- app/src/main/java/com/example/kalah/MainActivity.kt
+++ app/src/main/java/com/example/kalah/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.example.kalah
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -17,11 +23,34 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
 
-//    ВЕРХНЕЕ МЕНЮ
-    val tm_set: ImageButton = findViewById(R.id.btn_tm_set)
-    val tm_info: ImageButton = findViewById(R.id.btn_tm_info)
-    val tm_sound: ImageButton = findViewById(R.id.btn_tm_sound)
-    val tm_statistic: ImageButton = findViewById(R.id.btn_tm_statistic)
+
+        //ВЕРХНЕЕ МЕНЮ
+        val tm_set: ImageButton = findViewById(R.id.btn_tm_set)
+        val tm_info: ImageButton = findViewById(R.id.btn_tm_info)
+        val tm_sound: ImageButton = findViewById(R.id.btn_tm_sound)
+        val tm_statistic: ImageButton = findViewById(R.id.btn_tm_statistic)
+
+        //ОСНОВНЫЕ КНОПКИ
+        val btn_play_tog: ImageButton = findViewById(R.id.im_btn_play_together)
+        val btn_play_ai: ImageButton = findViewById(R.id.im_btn_play_ai)
+        val btn_setting: ImageButton = findViewById(R.id.im_btn_settings)
+
+
+
+        //КНОПКА ИНФОРМАЦИОННАЯ - ОБ АВТОРАХ
+        tm_info.setOnClickListener {
+            val dialog = Dialog(this)
+            dialog.setContentView(R.layout.info_dialog)
+
+            dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+            val okText = dialog.findViewById<TextView>(R.id.dialog_button)
+            okText.setOnClickListener {
+                dialog.dismiss()  // Только закрываем окно
+            }
+            dialog.show()  // Показываем диалог ВНЕ обработчика!
+        }
+
+    }
 }
